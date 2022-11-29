a192e9de2c2e5d817c9b99c2133d66e1ff69d534
piotrullo <wycieczkax@gmail.com>
2022-11-29 19:45:12 +0100

    style dodane :)

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -6,7 +6,7 @@ export const Home = () => {
     return <>
         <Container className="main centered">
             <Container className="header center-grid">
-                <Header size="1" id="home-header" className="">Home site</Header>
+                <Header size="1" id="home-header" className="">BUŁA DO RĘKI</Header>
             </Container>
             <Container className="center-grid">
                 <Items/>
