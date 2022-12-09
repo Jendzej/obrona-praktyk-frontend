2b5bb18b3abc0251188527786828a405242a5510
Jędrzej Runowicz <jedrzej.runowicz@gmail.com>
2022-12-09 12:19:32 +0100

    Fixes: add to shopping cart button fix

--- app/src/components/api_related/SingleItem.jsx
+++ app/src/components/api_related/SingleItem.jsx
@@ -1,31 +1,33 @@
 import {Container} from "../Container";
 import {Header} from "../Header";
 import {Button} from "../Button";
+import {parseJWT} from "../../utilities/parseJWT";
 
 export const SingleItem = ({id, item_name, item_price, item_description, item_image_url}) => {
-    const logged = localStorage.getItem('logged')
+    const logged = (localStorage.getItem('logged') === 'true') && (parseJWT(localStorage.getItem('jwt-token')).exp > rounded_date)
+
     return <>
         <Container id={id} className="single-item">
-                <Header size={2}>
-                    {item_name}
-                </Header>
-                <p className="item-description">{item_description}</p>
-                <img className="image" src={item_image_url} alt={item_name}/>
-                <p className="item-price"><b>Cena:</b> {item_price}zł</p>
-                {logged === 'true' ? <Container className="add-to-cart">
-                        <Button onClick={() => {
-                            if (localStorage.getItem('shopping-cart') === null) {
-                                localStorage.setItem('shopping-cart', JSON.stringify([]))
-                            }
-                            const data = JSON.parse(localStorage.getItem('shopping-cart'))
-                            data.push({id: id, price: item_price})
-                            localStorage.setItem('shopping-cart', JSON.stringify(data))
-                            alert(`Produkt '${item_name}' został dodany do koszyka!`)
-                        }}>
-                            Dodaj do koszyka
-                        </Button></Container>
-                    : <Container></Container>
-                }
+            <Header size={2}>
+                {item_name}
+            </Header>
+            <p className="item-description">{item_description}</p>
+            <img className="image" src={item_image_url} alt={item_name}/>
+            <p className="item-price"><b>Cena:</b> {item_price}zł</p>
+            {logged ? <Container className="add-to-cart">
+                    <Button onClick={() => {
+                        if (localStorage.getItem('shopping-cart') === null) {
+                            localStorage.setItem('shopping-cart', JSON.stringify([]))
+                        }
+                        const data = JSON.parse(localStorage.getItem('shopping-cart'))
+                        data.push({id: id, price: item_price})
+                        localStorage.setItem('shopping-cart', JSON.stringify(data))
+                        alert(`Produkt '${item_name}' został dodany do koszyka!`)
+                    }}>
+                        Dodaj do koszyka
+                    </Button></Container>
+                : <Container></Container>
+            }
 
         </Container>
     </>
